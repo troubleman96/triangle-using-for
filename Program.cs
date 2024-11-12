81ed8f7f9b7543d2a8a44834377e12badc8534f2
troubleman96 <itslugenge96@gmail.com>
2024-11-12 13:56:36 +0300

    Uno

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program{
+
+    static void Main(string [] args){
+
+       int heightOfTriangle = 5;
+
+       for(int i = 1; i <= heightOfTriangle; i++){
+
+            for(int j = 1; j <= i; j++){
+                System.Console.Write("*");
+            }
+            System.Console.WriteLine();
+       }
+
+    }
+}
